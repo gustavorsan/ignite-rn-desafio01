24a1cea2fe03c91bef1ecde453f373493b360f72
gustavorsan <gustavorsantos42@gmail.com>
2022-05-29 14:10:37 -0300

    Solução do desafio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,46 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 
 import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    
+
+
+
+    const task:Task  = {
+      id:  new Date().getTime(),
+      title : newTaskTitle,
+      done: false
+    }
+
+    setTasks([...tasks,task]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+      const updatedTasks = tasks.map(item => ({...item}));
+      
+      const foundItem = updatedTasks.find(item => item.id === id);
+
+      if(!foundItem)
+        return
+      
+      foundItem.done = !foundItem.done;
+
+      setTasks(updatedTasks);
+
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const filteredTasks = tasks.filter(item => item.id !== id);
+    setTasks(filteredTasks);
   }
 
   return (
